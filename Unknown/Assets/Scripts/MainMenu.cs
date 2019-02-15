ebe15effb201ae82e7b2bab7b688eb6c09b53130
xKuzdix <37182203+xKuzdix@users.noreply.github.com>
2019-02-15 15:18:52 -0500

    Add files via upload

---
+++ Unknown/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Button newGameButton;
+//    public Button loadButton;
+    public Button quitGameButton;
+    public string newGameSceneName;
+
+//    public GameObject loadGame;
+
+    public bool isPlay;
+    public bool isQuit;
+
+
+    public void NewGame() {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+/*    public void OpenloadGame() {
+      Debug.Log("what is thisssss");
+      loadGame.SetActive(true);
+    }
+*/
+
+    public void QuitGame() {
+      Application.Quit();
+    }
+
+
+    public void clickListener(){
+      newGameButton.onClick.AddListener(NewGame);
+      quitGameButton.onClick.AddListener(QuitGame);
+    }
+
+
+
+    void OnMouseUp(){
+      Debug.Log("clickajnvsdkbsvclick");
+  	if (isQuit)
+  	{
+      Debug.Log("quitttt");
+  		Application.Quit();
+  	}
+    if(isPlay)
+  	{
+      Debug.Log("playyyyyyy");
+  		NewGame();
+  	}
+  }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      isPlay = false;
+      isQuit = false;
+
+      clickListener();
+      OnMouseUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
